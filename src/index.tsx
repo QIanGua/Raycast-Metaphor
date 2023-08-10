a913dae985aa10a3bb4895138a07f340bc05b696
Qiangua <15757826110@163.com>
2023-08-10 14:31:16 +0800

    capture searchText & ignore 400 error

--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,61 @@
 import { useEffect, useState } from "react";
-import { Action, ActionPanel, List, getPreferenceValues, Icon, LaunchProps } from "@raycast/api";
+import { Action, ActionPanel, List, getPreferenceValues, Icon, showToast, Toast } from "@raycast/api";
 import Metaphor from 'metaphor-node';
+import { resolve } from "path";
 
 interface Preferences {
   MetaphorAPIKey: string;
 }
 
+const apikey = getPreferenceValues<Preferences>().MetaphorAPIKey;
+// console.log(apikey)
+const metaphor = new Metaphor(apikey);
+
 export default function Command() {
-  const [searchText, setSearchText] = useState();
+  const [searchText, setSearchText] = useState('');
   const [results, setResults] = useState([]);
-
-  const apikey = getPreferenceValues<Preferences>().MetaphorAPIKey;
-  // console.log(apikey)
-  const metaphor = new Metaphor(apikey);
-
+  const [loading, setLoading] = useState(false); 
+  
   useEffect(() => {
-    const searchArticles = async () => {
-      const  response = await metaphor.search(searchText, {
-      // const  response = await metaphor.search('funny article about tech culture', {
-        // 搜索选项 
-        numResults: 10,
-        useAutoprompt: true,
-        // includeDomains: ['nytimes.com', 'wsj.com'],
-        // startPublishedDate: '2023-06-12'
-      });
-      setResults(response.results);
-      // console.log(response);
+    const delaySearch = () => {
+      // delay 2000 ms(2s)
+      setTimeout(() => {
+        searchArticles(searchText);
+      }, 1000);
     }
-    searchArticles();
+
+    const searchArticles = async () => {
+      setLoading(true); //  loading
+      try {
+        const response = await metaphor.search(searchText, {
+          numResults: 10,
+          useAutoprompt: true
+        });
+
+        setResults(response.results);
+      } catch (error) {
+        if (!error.message.includes('400')) {
+          console.log(error.message);
+          showToast({
+            style: Toast.Style.Failure,
+            title: "Something went wrong hhh",
+            message: error.message,
+          });
+        }
+      }
+      setLoading(false); // 搜索结束,关闭 loading
+    }
+    delaySearch();
   }, [searchText]);
 
   return (
     <List
-    // searchText={searchText}
-    // onSearchTextChange={setSearchText}
+      isLoading={loading}
+      searchText={searchText}
+      navigationTitle="Search Results"
+      onSearchTextChange={setSearchText}
     >
-      {results.map(result => (
+      {results.length > 0 && results.map(result => (
         <List.Item
           key={result.id}
           icon={Icon.Stars}

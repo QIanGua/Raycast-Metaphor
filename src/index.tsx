e8696547c01bfd7998a8984ddce7dcbacc68c2cc
Qiangua <15757826110@163.com>
2023-09-11 00:50:42 +0800

    update icon and bounce input

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
-import { useEffect, useState } from "react";
-import { Action, ActionPanel, List, getPreferenceValues, Icon, showToast, Toast } from "@raycast/api";
+import { useEffect, useState, useRef} from "react";
+import { Action, ActionPanel, List, Image, getPreferenceValues, showToast, Toast } from "@raycast/api";
+import { getFavicon } from "@raycast/utils";
 import Metaphor from 'metaphor-node';
-import { resolve } from "path";
 
 interface Preferences {
   MetaphorAPIKey: string;
 }
+
+interface Result {
+  title: string; // The title of the search result.
+  url: string; // The URL of the search result.
+  publishedDate?: string; // The estimated creation date of the content. Format is YYYY-MM-DD. Nullable
+  author?: string; // The author of the content, if available. Nullable
+  score?: number; // A number from 0 to 1 representing similarity between the query/url and the result.
+  id: string; // The temporary ID for the document. Useful for /contents endpoint.
+}
+
 
 const apikey = getPreferenceValues<Preferences>().MetaphorAPIKey;
 // console.log(apikey)
@@ -13,19 +23,21 @@ const metaphor = new Metaphor(apikey);
 
 export default function Command() {
   const [searchText, setSearchText] = useState('');
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState<Result[]>([]);
   const [loading, setLoading] = useState(false); 
-  
+  const timerRef = useRef<NodeJS.Timeout | null>(null); 
   useEffect(() => {
-    const delaySearch = () => {
-      // delay 2000 ms(2s)
-      setTimeout(() => {
-        searchArticles(searchText);
-      }, 1000);
+
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
     }
 
+    timerRef.current = setTimeout(() => {
+      searchArticles();
+    }, 800);
+
     const searchArticles = async () => {
-      setLoading(true); //  loading
+      setLoading(true); // 
       try {
         const response = await metaphor.search(searchText, {
           numResults: 10,
@@ -34,18 +46,18 @@ export default function Command() {
 
         setResults(response.results);
       } catch (error) {
-        if (!error.message.includes('400')) {
-          console.log(error.message);
+        const err = error as Error;
+        if (!err.message.includes('400')) {
+          console.log(err.message);
           showToast({
             style: Toast.Style.Failure,
             title: "Something went wrong hhh",
-            message: error.message,
+            message: err.message,
           });
         }
       }
-      setLoading(false); // 搜索结束,关闭 loading
+      setLoading(false); 
     }
-    delaySearch();
   }, [searchText]);
 
   return (
@@ -58,8 +70,8 @@ export default function Command() {
       {results.length > 0 && results.map(result => (
         <List.Item
           key={result.id}
-          icon={Icon.Stars}
-          title={result.title}
+          icon={getFavicon(result.url, { mask: Image.Mask.RoundedRectangle })}
+          title={result.title ? result.title : "Empty Title"}
           subtitle={result.author}
           actions={
             <ActionPanel>

d8daa0f68190cf20f40378ae344f98cd22591f79
Qiangua <15757826110@163.com>
2023-09-11 19:56:31 +0800

    add type.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,7 @@ import { useEffect, useState, useRef} from "react";
 import { Action, ActionPanel, List, Image, getPreferenceValues, showToast, Toast } from "@raycast/api";
 import { getFavicon } from "@raycast/utils";
 import Metaphor from 'metaphor-node';
-
-interface Preferences {
-  MetaphorAPIKey: string;
-}
-
-interface Result {
-  title: string; // The title of the search result.
-  url: string; // The URL of the search result.
-  publishedDate?: string; // The estimated creation date of the content. Format is YYYY-MM-DD. Nullable
-  author?: string; // The author of the content, if available. Nullable
-  score?: number; // A number from 0 to 1 representing similarity between the query/url and the result.
-  id: string; // The temporary ID for the document. Useful for /contents endpoint.
-}
+import { Preferences, Result } from './type'
 
 
 const apikey = getPreferenceValues<Preferences>().MetaphorAPIKey;
